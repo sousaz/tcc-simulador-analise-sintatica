e8378a56975a5db84a11d66a844a8a107810f51e
Rogerio Crestani <rogerio.crestani1@gmail.com>
2023-10-25 08:48:51 -0300

    Atualização hospedagem v0.2

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,65 @@
+# Importacoes
 import uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
-if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="localhost", port=8000, reload=True)
+# Importar funcoes
+from app import parsing_table
+from app import parsing_algorithm
+from app import utils
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000",
+    "http://localhost:5173",
+    "localhost:5173",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Testar API
+@app.get("/")
+async def home() -> dict:
+    return {"boas-vindas": "Bem-vindo a API do SASC."}
+
+
+# Testar API
+@app.get("/test/")
+async def read_root() -> dict:
+    return {"message": "Testando api"}
+
+
+@app.get("/analyze/{grammar}/{input}/{analysis_type}")
+async def get_table(input: str, grammar: str, analysis_type: str):
+    try:
+        new_grammar = utils.grammar_formatter(grammar)
+        treated_grammar = utils.symbol_treat(grammar)
+
+        goto_action_tables = parsing_table.get_goto_action_tables(
+            treated_grammar, analysis_type
+        )
+
+        steps_parsing = parsing_algorithm.bottom_up_algorithm(
+            utils.dict_treat(goto_action_tables["action_table"]),
+            utils.dict_treat(goto_action_tables["goto_table"]),
+            input,
+        )
+
+        return {
+            "ERROR_CODE": 0,
+            "parsingTable": goto_action_tables,
+            "stepsParsing": steps_parsing,
+            "grammar": new_grammar,
+        }
+    except Exception as e:
+        return {"ERROR_CODE": 1, "errorMessage": f"Houve um erro! {e}"}
