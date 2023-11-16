878fe5a55a01c77ee5e42860a5123ee128507dd7
Rogerio Crestani <rogerio.crestani1@gmail.com>
2023-11-15 22:33:19 -0300

    Adição de rodapé e algumas correções.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import "intro.js/introjs.css";
 import "./index.css";
 
 import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
